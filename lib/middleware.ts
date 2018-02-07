eab0a37033ae8f2276e775a1159b50d4ef939516
Jingkun Hua <huajingkun1214@gmail.com>
2018-02-07 17:25:22 +0800

    refactor: simplify middlewares.ts

--- lib/middleware.ts
+++ lib/middleware.ts
@@ -3,7 +3,8 @@ import { Context } from 'egg'
 export type IMiddleware = (ctx: Context, next: () => Promise<any>) => any
 export type ClassOrPropertyDecorator = (target: any, key?: string | symbol) => any
 
-export function Middleware (middleware: IMiddleware, name: string = middleware.name): ClassOrPropertyDecorator {
+/** Insert a middleware into this route */
+export function Middleware (middleware: IMiddleware): ClassOrPropertyDecorator {
   return function MiddlewareDecorator (target, key) {
     const func = key ? target[key] : target
     const middlewares: IMiddleware[] = func.middlewares || (func.middlewares = [])
@@ -11,6 +12,5 @@ export function Middleware (middleware: IMiddleware, name: string = middleware.n
   }
 }
 
-export function createDecorator (middleware: IMiddleware): ClassOrPropertyDecorator {
-  return Middleware(middleware)
-}
+/** Create a middleware decorator */
+export const createDecorator = Middleware
