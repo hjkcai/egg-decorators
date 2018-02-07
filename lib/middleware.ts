34af5e4d3ab37f99b8d0a70f660144b15f67ed16
Jingkun Hua <huajingkun1214@gmail.com>
2018-02-07 16:26:17 +0800

    refactor: rename IDecorator -> ClassOrPropertyDecorator

--- lib/middleware.ts
+++ lib/middleware.ts
@@ -1,9 +1,9 @@
 import { Context } from 'egg'
 
 export type IMiddleware = (ctx: Context, next: () => Promise<any>) => any
-export type IDecorator = (target: any, key?: string | symbol) => any
+export type ClassOrPropertyDecorator = (target: any, key?: string | symbol) => any
 
-export function Middleware (middleware: IMiddleware, name: string = middleware.name): IDecorator {
+export function Middleware (middleware: IMiddleware, name: string = middleware.name): ClassOrPropertyDecorator {
   return function MiddlewareDecorator (target, key) {
     const func = key ? target[key] : target
     const middlewares: IMiddleware[] = func.middlewares || (func.middlewares = [])
@@ -11,6 +11,6 @@ export function Middleware (middleware: IMiddleware, name: string = middleware.n
   }
 }
 
-export function createDecorator (middleware: IMiddleware): IDecorator {
+export function createDecorator (middleware: IMiddleware): ClassOrPropertyDecorator {
   return Middleware(middleware)
 }
